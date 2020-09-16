f69ff9da879e49c11defc2199053a3bf39edae49
Liam Adams <liam_adams@mckinsey.com>
2020-09-15 23:03:37 -0400

    updates

--- hw2/xor.py
+++ hw2/xor.py
@@ -1,90 +1,24 @@
-import dimod
-from dwavebinarycsp.core.constraint import Constraint
-from dwavebinarycsp.core.csp import ConstraintSatisfactionProblem
-from dwavebinarycsp import stitch
-import neal
+'''
+Liam Adams - lbadams2
+'''
 
-@dimod.decorators.vartype_argument('vartype')
-def nor_gate(variables, vartype=dimod.BINARY, name='NOR'):
-    """NOR gate.
+from dwave.system.samplers import DWaveSampler
+from dwave.system.composites import EmbeddingComposite
+sampler = DWaveSampler()
+sampler_embedded = EmbeddingComposite(sampler)
 
-    Args:
-        variables (list): Variable labels for the and gate as `[in1, in2, out]`,
-            where `in1, in2` are inputs and `out` the gate's output.
-        vartype (Vartype, optional, default='BINARY'): Variable type. Accepted
-            input values:
+Q = {('q1','q2'): 1, ('q1','q3'): 2, ('q2','q3'): 2, ('q1','q1'): -1, ('q2','q2'): -1, ('q3','q3'): -1, # q1 NOR q2 = q3
+     ('q1','q3'): 1, ('q1','q4'): 2, ('q3','q4'): 2, ('q4','q4'): -1, # q1 NOR q3 = q4
+     ('q2','q3'): 1, ('q2','q5'): 2, ('q3','q5'): 2, ('q5','q5'): -1, # q2 NOR q3 = q5
+     ('q4','q5'): 1, ('q4','q6'): 2, ('q5','q6'): 2, ('q6','q6'): -1, # q4 NOR q5 = q6
+    
+    # duplicate q6
+     ()
 
-            * Vartype.SPIN, 'SPIN', {-1, 1}
-            * Vartype.BINARY, 'BINARY', {0, 1}
-        name (str, optional, default='NOR'): Name for the constraint.
+     ('q6','q7'): 1, ('q6','q8'): 2, ('q7','q8'): 2, ('q8','q8'): -1, # q6 NOR q7 = q8
+    
+    }
 
-    Returns:
-        Constraint(:obj:`.Constraint`): Constraint that is satisfied when its variables are
-        assigned values that match the valid states of an NOR gate.
-
-    Examples:
-        >>> import dwavebinarycsp.factories.constraint.gates as gates
-        >>> csp = dwavebinarycsp.ConstraintSatisfactionProblem(dwavebinarycsp.SPIN)
-        >>> csp.add_constraint(gates.nor_gate(['x', 'y', 'z'], {-1,1}, name='NOR1'))
-        >>> csp.check({'x': 1, 'y': -1, 'z': -1})
-        True
-    """
-
-    variables = tuple(variables)
-
-    if vartype is dimod.BINARY:
-        configs = frozenset([(0, 0, 1),
-                             (0, 1, 0),
-                             (1, 0, 0),
-                             (1, 1, 0)])
-
-        def func(in1, in2, out): return ((not in1) and (not in2)) == out
-
-    else:
-        # SPIN, vartype is checked by the decorator
-        configs = frozenset([(-1, -1, +1),
-                             (-1, +1, -1),
-                             (+1, -1, -1),
-                             (+1, +1, -1)])
-
-        def func(in1, in2, out): return ((in1 < 0) and (in2 < 0)) == (out > 0)
-
-    return Constraint(func, configs, variables, vartype=vartype, name=name)
-
-
-
-def xor_circuit(vartype=dimod.BINARY):
-    # also checks the vartype argument
-    csp = ConstraintSatisfactionProblem(vartype)
-
-    gate = nor_gate(['in1', 'in2', 'out1'], vartype=vartype, name='NOR(in1, in2) = out1')
-    csp.add_constraint(gate)
-
-    gate = nor_gate(['in1', 'out1', 'out2'], vartype=vartype, name='NOR(in1, out1) = out2')
-    csp.add_constraint(gate)
-
-    gate = nor_gate(['in2', 'out1', 'out3'], vartype=vartype, name='NOR(in2, out1) = out3')
-    csp.add_constraint(gate)
-
-    gate = nor_gate(['out2', 'out3', 'out4'], vartype=vartype, name='NOR(out2, out3) = out4')
-    csp.add_constraint(gate)
-
-    gate = nor_gate(['out4', 'out4', 'out5'], vartype=vartype, name='NOR(out4, out4) = out5')
-    csp.add_constraint(gate)
-
-    return csp
-
-
-def main():
-    csp = xor_circuit()
-    bqm = stitch(csp)
-    bqm.fix_variable('in1', 1)
-    bqm.fix_variable('in2', 0)
-    sampler = neal.SimulatedAnnealingSampler()
-    response = sampler.sample(bqm)
-    p = next(response.samples(n=1, sorted_by='energy'))
-    print(p)
-
-
-if __name__ == '__main__':
-    main()
+response = sampler_embedded.sample_qubo(Q, num_reads=5000)
+for datum in response.data(['sample', 'energy', 'num_occurrences']):   
+  print(datum.sample, "Energy: ", datum.energy, "Occurrences: ", datum.num_occurrences)
