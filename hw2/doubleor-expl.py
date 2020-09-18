a1605c5195125102ddb9bb2889d24e21d54b8830
Liam Adams <liam_adams@mckinsey.com>
2020-09-17 22:51:51 -0400

    prob 1 mostly working

--- hw2/doubleor-expl.py
+++ hw2/doubleor-expl.py
@@ -4,22 +4,47 @@ Liam Adams - lbadams2
 
 from dwave.system.samplers import DWaveSampler
 from dwave.system.composites import FixedEmbeddingComposite
-embedding = {'q1': {0}, 'q2': {4}, 'q3': {1}, 'q4': {2}, 'q5': {3}, 'q6': {5}, 'q7': {6}}
-sampler = DWaveSampler()
-sampler_embedded = FixedEmbeddingComposite(sampler, embedding)
-Q = {('q1','q2'): 1, ('q1','q6'): 2, ('q2','q3'): 2, ('q4','q6'): 1, ('q5','q6'): 2, \
-    ('q4','q7'): 2, ('q3','q6'): -2, ('q5','q7'): -2, ('q1','q1'): -1, ('q2','q2'): -1, \
-        ('q3','q3'): .5, ('q4','q4'): -1, ('q5','q5'): .5, ('q6','q6'): .5, ('q7','q7'): .5}
-response = sampler_embedded.sample_qubo(Q, num_reads=5000)
-for datum in response.data(['sample', 'energy', 'num_occurrences']):   
-  print(datum.sample, "Energy: ", datum.energy, "Occurrences: ", datum.num_occurrences)
+from dimod import ExactSolver
 
+
+def run_on_hw(Q, embedding):
+    sampler = DWaveSampler()
+    sampler_embedded = FixedEmbeddingComposite(sampler, embedding)
+    response = sampler_embedded.sample_qubo(Q, num_reads=5000)
+    for datum in response.data(['sample', 'energy', 'num_occurrences']):   
+        print(datum.sample, "Energy: ", datum.energy, "Occurrences: ", datum.num_occurrences)
+
+def run_on_sim(Q, embedding):
+    sampler = ExactSolver()
+    sampler_embedded = FixedEmbeddingComposite(sampler, embedding)
+    response = sampler_embedded.sample_qubo(Q)
+    for datum in response.data(['sample', 'energy', 'num_occurrences']):
+        print(datum.sample, 'Energy: ', datum.energy, "Occurrences: ", datum.num_occurrences)
+
+
+def run():
+    embedding = {'q1': {0}, 'q2': {4}, 'q3': {5}, 'q4': {1}, 'q5': {7}, 'q6': {6}, 'q7': {2}}
+    Q = {('q1','q2'): 1, ('q1','q3'): 2, ('q2','q4'): 2, ('q3','q4'): -2, ('q4','q5'): 1, \
+        ('q4','q6'): 2, ('q5','q7'): 2, ('q1','q1'): -1, ('q2','q2'): -1, ('q3','q3'): .5, \
+            ('q4','q4'): -.5, ('q5','q5'): -1, ('q6','q6'): .5, ('q7','q7'): .5}
+    run_on_hw(Q, embedding) 
+
+
+if __name__ == "__main__":
+    run()
 
 '''
-{'q1': 0, 'q2': 1, 'q3': 0, 'q4': 0, 'q5': 1, 'q6': 0, 'q7': 1} Energy:  -2.0 Occurrences:  647
-{'q1': 1, 'q2': 0, 'q3': 0, 'q4': 0, 'q5': 1, 'q6': 0, 'q7': 1} Energy:  -2.0 Occurrences:  316
-{'q1': 1, 'q2': 1, 'q3': 0, 'q4': 1, 'q5': 0, 'q6': 0, 'q7': 0} Energy:  -2.0 Occurrences:  1362
-{'q1': 0, 'q2': 1, 'q3': 0, 'q4': 1, 'q5': 0, 'q6': 0, 'q7': 0} Energy:  -2.0 Occurrences:  1319
-{'q1': 1, 'q2': 1, 'q3': 0, 'q4': 0, 'q5': 1, 'q6': 0, 'q7': 1} Energy:  -2.0 Occurrences:  838
-{'q1': 1, 'q2': 0, 'q3': 0, 'q4': 1, 'q5': 0, 'q6': 0, 'q7': 0} Energy:  -2.0 Occurrences:  518
+Output from hardware
+q3 and q4 are coupled, they are the output from q1 NOR q2
+q6 and q7 are coupled, they are the output from q4 NOR q5
+
+{'q1': 0, 'q2': 1, 'q3': 0, 'q4': 0, 'q5': 1, 'q6': 0, 'q7': 0} Energy:  -2.0 Occurrences:  763
+{'q1': 1, 'q2': 0, 'q3': 0, 'q4': 0, 'q5': 1, 'q6': 0, 'q7': 0} Energy:  -2.0 Occurrences:  366
+{'q1': 1, 'q2': 1, 'q3': 0, 'q4': 0, 'q5': 1, 'q6': 0, 'q7': 0} Energy:  -2.0 Occurrences:  916
+{'q1': 0, 'q2': 0, 'q3': 1, 'q4': 1, 'q5': 1, 'q6': 0, 'q7': 0} Energy:  -2.0 Occurrences:  1000
+{'q1': 0, 'q2': 0, 'q3': 1, 'q4': 1, 'q5': 0, 'q6': 0, 'q7': 0} Energy:  -2.0 Occurrences:  1949
+{'q1': 0, 'q2': 1, 'q3': 1, 'q4': 0, 'q5': 1, 'q6': 0, 'q7': 0} Energy:  -1.5 Occurrences:  1
+{'q1': 0, 'q2': 0, 'q3': 1, 'q4': 1, 'q5': 0, 'q6': 0, 'q7': 1} Energy:  -1.5 Occurrences:  2
+{'q1': 1, 'q2': 0, 'q3': 0, 'q4': 1, 'q5': 1, 'q6': 0, 'q7': 0} Energy:  -1.5 Occurrences:  2
+{'q1': 1, 'q2': 1, 'q3': 0, 'q4': 0, 'q5': 1, 'q6': 1, 'q7': 0} Energy:  -1.5 Occurrences:  1
 '''
--- hw2/doubleor-impl.py
+++ hw2/doubleor-impl.py
@@ -4,25 +4,77 @@ Liam Adams - lbadams2
 
 from dwave.system.samplers import DWaveSampler
 from dwave.system.composites import EmbeddingComposite
-sampler = DWaveSampler()
-sampler_embedded = EmbeddingComposite(sampler)
-Q = {('q1','q2'): 1, ('q1','q3'): 2, ('q1','q4'): 0, ('q1','q5'): 0, ('q2','q3'): 2, ('q2','q4'): 0, ('q2','q5'): 0, \
-    ('q3','q4'): 1, ('q3','q5'): 2, ('q4','q5'): 2, ('q1','q1'): -1, ('q2','q2'): -1, \
-        ('q3','q3'): -1, ('q4','q4'): -1, ('q5','q5'): -1}
-response = sampler_embedded.sample_qubo(Q, num_reads=5000)
-for datum in response.data(['sample', 'energy', 'num_occurrences']):   
-  print(datum.sample, "Energy: ", datum.energy, "Occurrences: ", datum.num_occurrences)
+from dimod import ExactSolver
+import sys
 
 
+def run_on_hw(Q):
+    sampler = DWaveSampler()
+    sampler_embedded = EmbeddingComposite(sampler)
+    response = sampler_embedded.sample_qubo(Q, num_reads=5000)
+    for datum in response.data(['sample', 'energy', 'num_occurrences']):   
+        print(datum.sample, "Energy: ", datum.energy, "Occurrences: ", datum.num_occurrences)
+
+def run_on_sim(Q):
+    sampler = ExactSolver()
+    response = sampler.sample_qubo(Q)
+    for datum in response.data(['sample', 'energy', 'num_occurrences']):
+        print(datum.sample, 'Energy: ', datum.energy, "Occurrences: ", datum.num_occurrences)
+
+
+def hardcoded():
+    Q = {('q1','q2'): 1, ('q1','q3'): 2, ('q2','q3'): 2, \
+        ('q3','q4'): 1, ('q3','q5'): 2, ('q4','q5'): 2, ('q1','q1'): -1, ('q2','q2'): -1, \
+            ('q3','q3'): -2, ('q4','q4'): -1, ('q5','q5'): -1}
+    run_on_sim(Q)
+
+
+def create_nor_qubo(num_gates):
+    # if n=4 there will be 2 gates and an additional qubit for the output of the second gate
+    Q = {}
+    for i in range(num_gates + 1):
+        i += 1 # start at 1
+        qubit_name = 'q' + str(i)
+        # every qubit should have -1 weight
+        Q[(qubit_name, qubit_name)] = -1
+        
+        # inputs are 1, 2, 4, 6, ...
+        # edges between inputs should be 1
+        if i % 2 == 0:
+            other_qubit = 'q' + str(i-1)
+            Q[(qubit_name, other_qubit)] = 1
+
+        # outputs will be 3, 5, ..., num_gates + 1
+        # edges between inputs and outputs should be 2
+        if i % 2 == 1 and i > 1: # odd qubit will have edges to previous 2 qubits
+            other_qubit = 'q' + str(i-1)
+            Q[(qubit_name, other_qubit)] = 2
+            other_qubit = 'q' + str(i-2)
+            Q[(qubit_name, other_qubit)] = 2
+
+    run_on_sim(Q)
+
+
+if __name__ == '__main__':
+    #n = int(sys.argv[1])
+    #create_nor_qubo(n)
+    hardcoded()
 
 '''
-{'q1': 1, 'q2': 1, 'q3': 0, 'q4': 0, 'q5': 1} Energy:  -2.0 Occurrences:  1211
-{'q1': 0, 'q2': 1, 'q3': 0, 'q4': 1, 'q5': 0} Energy:  -2.0 Occurrences:  564
-{'q1': 1, 'q2': 1, 'q3': 0, 'q4': 1, 'q5': 0} Energy:  -2.0 Occurrences:  898
-{'q1': 0, 'q2': 1, 'q3': 0, 'q4': 0, 'q5': 1} Energy:  -2.0 Occurrences:  843
-{'q1': 1, 'q2': 0, 'q3': 0, 'q4': 1, 'q5': 0} Energy:  -2.0 Occurrences:  673
-{'q1': 1, 'q2': 0, 'q3': 0, 'q4': 0, 'q5': 1} Energy:  -2.0 Occurrences:  805
-{'q1': 1, 'q2': 1, 'q3': 0, 'q4': 1, 'q5': 1} Energy:  -1.0 Occurrences:  1
-{'q1': 0, 'q2': 0, 'q3': 1, 'q4': 0, 'q5': 0} Energy:  -1.0 Occurrences:  4
-{'q1': 0, 'q2': 0, 'q3': 1, 'q4': 0, 'q5': 1} Energy:  0.0 Occurrences:  1
+Output on hardware
+
+{'q1': 1, 'q2': 1, 'q3': 0, 'q4': 1, 'q5': 0} Energy:  -2.0 Occurrences:  997
+{'q1': 0, 'q2': 0, 'q3': 1, 'q4': 1, 'q5': 0} Energy:  -2.0 Occurrences:  1087
+{'q1': 1, 'q2': 0, 'q3': 0, 'q4': 0, 'q5': 1} Energy:  -2.0 Occurrences:  234
+{'q1': 1, 'q2': 1, 'q3': 0, 'q4': 0, 'q5': 1} Energy:  -2.0 Occurrences:  585
+{'q1': 0, 'q2': 1, 'q3': 0, 'q4': 0, 'q5': 1} Energy:  -2.0 Occurrences:  204
+{'q1': 0, 'q2': 0, 'q3': 1, 'q4': 0, 'q5': 0} Energy:  -2.0 Occurrences:  617
+{'q1': 1, 'q2': 0, 'q3': 0, 'q4': 1, 'q5': 0} Energy:  -2.0 Occurrences:  553
+{'q1': 0, 'q2': 0, 'q3': 1, 'q4': 0, 'q5': 0} Energy:  -2.0 Occurrences:  334
+{'q1': 0, 'q2': 1, 'q3': 0, 'q4': 1, 'q5': 0} Energy:  -2.0 Occurrences:  368
+{'q1': 0, 'q2': 0, 'q3': 1, 'q4': 1, 'q5': 0} Energy:  -2.0 Occurrences:  9
+{'q1': 1, 'q2': 1, 'q3': 0, 'q4': 0, 'q5': 1} Energy:  -2.0 Occurrences:  1
+{'q1': 1, 'q2': 0, 'q3': 0, 'q4': 0, 'q5': 0} Energy:  -1.0 Occurrences:  3
+{'q1': 1, 'q2': 0, 'q3': 0, 'q4': 0, 'q5': 0} Energy:  -1.0 Occurrences:  1
+{'q1': 1, 'q2': 1, 'q3': 0, 'q4': 1, 'q5': 1} Energy:  1.0 Occurrences:  7
 '''
