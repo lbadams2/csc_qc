b5d1202e873b8c0288e7e2582ceeca5422b8a84f
Liam Adams <liam_adams@mckinsey.com>
2020-09-27 10:52:28 -0400

    hw3 p1 done

--- hw2/xor3.py
+++ hw2/xor3.py
@@ -4,97 +4,46 @@ Liam Adams - lbadams2
 
 from dwave.system.samplers import DWaveSampler
 from dwave.system.composites import EmbeddingComposite
-from dimod import ExactSolver
+sampler = DWaveSampler()
+sampler_embedded = EmbeddingComposite(sampler)
 
-def run_on_hw(Q):
-    sampler = DWaveSampler()
-    sampler_embedded = EmbeddingComposite(sampler)
-    response = sampler_embedded.sample_qubo(Q, num_reads=5000)
-    for datum in response.data(['sample', 'energy', 'num_occurrences']):   
-        print(datum.sample, "Energy: ", datum.energy, "Occurrences: ", datum.num_occurrences)
+Q = {('q1','q2'): 1, ('q1','q3'): 2, ('q2','q3'): 2, ('q1','q1'): -1, ('q2','q2'): -1, ('q3','q3'): -1, # q1 NOR q2 = q3
+     ('q1','q3'): 1, ('q1','q4'): 2, ('q3','q4'): 2, ('q4','q4'): -1, # q1 NOR q3 = q4
+     ('q2','q3'): 1, ('q2','q5'): 2, ('q3','q5'): 2, ('q5','q5'): -1, # q2 NOR q3 = q5
+     ('q4','q5'): 1, ('q4','q6'): 2, ('q5','q6'): 2,  # q4 NOR q5 = q6
+    # above valid except missing (q6,q6): -1
 
-def run_on_sim(Q):
-    sampler = ExactSolver()
-    response = sampler.sample_qubo(Q)
-    for datum in response.data(['sample', 'energy', 'num_occurrences']):
-        print(datum.sample, 'Energy: ', datum.energy, "Occurrences: ", datum.num_occurrences)
+    # duplicate q6
+     #('q6','q7'): -2, ('q6','q6'): .5, ('q7','q7'): .5,
+     ('q6','q6'): .5, ('q7','q7'): .5,
+    
+    # combine duplicate weights with NOR weights (q6,q7): -2 + (q6,q7): 1
+     ('q6','q7'): -1, ('q6','q8'): 2, ('q7','q8'): 2, ('q8','q8'): -1, # q6 NOR q7 = q8
+    }
 
+response = sampler_embedded.sample_qubo(Q, num_reads=5000)
+for datum in response.data(['sample', 'energy', 'num_occurrences']):   
+  print(datum.sample, "Energy: ", datum.energy, "Occurrences: ", datum.num_occurrences)
 
-def get_nor_qubo(qub_in_1, qub_in_2, qub_out):
-    qubo = {}
-    qubo[(qub_in_1, qub_in_1)] = -1
-    qubo[(qub_in_2, qub_in_2)] = -1
-    qubo[(qub_out, qub_out)] = -1
-    qubo[(qub_in_1, qub_in_2)] = 1
-    qubo[(qub_in_1, qub_out)] = 2
-    qubo[(qub_in_2, qub_out)] = 2
-    return qubo
-
-
-def duplicate_qubit(qubo, qubit_name, dup_qubit_name):
-    couple_weight = -2
-    qubit_weight = qubo[qubit_name]
-    split_weight = qubit_weight / 2 + (-1*(.5 * couple_weight))
-    qubo[qubit_name] = split_weight
-    qubo[(dup_qubit_name, dup_qubit_name)] = split_weight
-    qubo[qubit_name, dup_qubit_name] = couple_weight
-
-
-def run():
-    Q = {}
-    first_gate = get_nor_qubo('q1', 'q2', 'q3')
-    Q.update(first_gate)
-
-    # duplicate q1 to q4
-    duplicate_qubit(Q, 'q1', 'q4')
-
-    # duplicate q2 to q5
-    duplicate_qubit(Q, 'q2', 'q5')
-
-    # duplicate q3 to q6
-    duplicate_qubit(Q, 'q3', 'q6')
-
-    # q4 NOR q3 = q7
-    second_gate = get_nor_qubo('q3', 'q4', 'q7')
-
-
-
-
-def run_hardcoded():
-    Q = {('q1','q1'): -2, ('q2','q2'): -2, ('q3','q3'): -3, ('q4','q4'): -1, ('q5','q5'): -2, ('q6','q6'): -1, ('q7','q7'): -1, \
-        ('q8','q8'): -2, ('q1','q1'): -2, ('q1','q2'): 1, ('q1','q3'): 4, ('q1','q4'): 2, ('q2','q3'): 3, ('q2','q4'): -1, \
-            ('q2','q5'): 2, ('q3','q4'): 1, ('q3','q5'): 2, ('q4','q5'): 1, ('q4','q6'): 2, ('q4','q7'): 1, ('q5','q6'): 2, \
-            ('q5','q7'): 1, ('q6','q8'): 2, ('q7','q8'): 2}
-
-    '''
-    Q = {('q1','q1'): .5, ('q2','q2'): .5, ('q3','q3'): -.5, ('q4','q4'): -.5, ('q5','q5'): -.5, ('q6','q6'): -.5, ('q7','q7'): -2, \
-        ('q8','q8'): -2, ('q1','q2'): 1, ('q1','q3'): 2, ('q2','q3'): 2, ('q1','q4'): -2, ('q2','q5'): -2, ('q3','q6'): -2, \
-            ('q3','q4'): 1, ('q3','q7'): 2, ('q4','q7'): 2, ('q5','q6'): 1, ('q5','q8'): 2, ('q6','q8'): 2, ('q9','q9'): -.5, \
-                ('q10','q10'): -.5, ('q11','q11'): -1, ('q9','q10'): 1, ('q9','q11'): 2, ('q10','q11'): 2, ('q7','q8'): 1, \
-                    ('q7','q9'): 2, ('q8','q9'): 2, ('q9','q10'): -2}
-    '''
-
-    run_on_hw(Q)
-
-
-if __name__ == "__main__":
-    run_hardcoded()
 
 
 '''
-Output from hardware
-q1 XOR q2 = q8
-
-{'q1': 1, 'q2': 0, 'q3': 0, 'q4': 0, 'q5': 1, 'q6': 0, 'q7': 0, 'q8': 1} Energy:  -6.0 Occurrences:  3156
-{'q1': 0, 'q2': 1, 'q3': 0, 'q4': 1, 'q5': 0, 'q6': 0, 'q7': 0, 'q8': 1} Energy:  -6.0 Occurrences:  1794
-{'q1': 0, 'q2': 0, 'q3': 1, 'q4': 0, 'q5': 1, 'q6': 0, 'q7': 0, 'q8': 1} Energy:  -5.0 Occurrences:  14
-{'q1': 1, 'q2': 1, 'q3': 0, 'q4': 0, 'q5': 0, 'q6': 0, 'q7': 0, 'q8': 1} Energy:  -5.0 Occurrences:  2
-{'q1': 1, 'q2': 1, 'q3': 0, 'q4': 0, 'q5': 0, 'q6': 1, 'q7': 1, 'q8': 0} Energy:  -5.0 Occurrences:  2
-{'q1': 0, 'q2': 1, 'q3': 1, 'q4': 1, 'q5': 0, 'q6': 0, 'q7': 0, 'q8': 1} Energy:  -5.0 Occurrences:  1
-{'q1': 0, 'q2': 0, 'q3': 1, 'q4': 0, 'q5': 0, 'q6': 0, 'q7': 0, 'q8': 1} Energy:  -5.0 Occurrences:  6
-{'q1': 0, 'q2': 0, 'q3': 1, 'q4': 0, 'q5': 0, 'q6': 1, 'q7': 1, 'q8': 0} Energy:  -5.0 Occurrences:  17
-{'q1': 1, 'q2': 1, 'q3': 0, 'q4': 1, 'q5': 0, 'q6': 0, 'q7': 0, 'q8': 1} Energy:  -5.0 Occurrences:  1
-{'q1': 0, 'q2': 1, 'q3': 0, 'q4': 1, 'q5': 1, 'q6': 0, 'q7': 0, 'q8': 1} Energy:  -5.0 Occurrences:  4
-{'q1': 1, 'q2': 0, 'q3': 0, 'q4': 0, 'q5': 1, 'q6': 0, 'q7': 1, 'q8': 1} Energy:  -4.0 Occurrences:  2
-{'q1': 1, 'q2': 0, 'q3': 0, 'q4': 0, 'q5': 0, 'q6': 0, 'q7': 0, 'q8': 1} Energy:  -4.0 Occurrences:  1
+{'q1': 0, 'q2': 1, 'q3': 0, 'q4': 1, 'q5': 0, 'q6': 0, 'q7': 0, 'q8': 1} Energy:  -3.0 Occurrences:  2049
+{'q1': 1, 'q2': 0, 'q3': 0, 'q4': 0, 'q5': 1, 'q6': 0, 'q7': 0, 'q8': 1} Energy:  -3.0 Occurrences:  2869
+{'q1': 0, 'q2': 0, 'q3': 1, 'q4': 0, 'q5': 0, 'q6': 0, 'q7': 0, 'q8': 1} Energy:  -2.0 Occurrences:  15
+{'q1': 0, 'q2': 1, 'q3': 0, 'q4': 0, 'q5': 0, 'q6': 0, 'q7': 0, 'q8': 1} Energy:  -2.0 Occurrences:  7
+{'q1': 0, 'q2': 1, 'q3': 0, 'q4': 1, 'q5': 0, 'q6': 0, 'q7': 0, 'q8': 0} Energy:  -2.0 Occurrences:  2
+{'q1': 1, 'q2': 0, 'q3': 0, 'q4': 0, 'q5': 0, 'q6': 0, 'q7': 0, 'q8': 1} Energy:  -2.0 Occurrences:  9
+{'q1': 0, 'q2': 0, 'q3': 0, 'q4': 0, 'q5': 1, 'q6': 0, 'q7': 0, 'q8': 1} Energy:  -2.0 Occurrences:  2
+{'q1': 1, 'q2': 0, 'q3': 1, 'q4': 0, 'q5': 0, 'q6': 0, 'q7': 0, 'q8': 1} Energy:  -2.0 Occurrences:  13
+{'q1': 1, 'q2': 0, 'q3': 0, 'q4': 0, 'q5': 1, 'q6': 0, 'q7': 0, 'q8': 0} Energy:  -2.0 Occurrences:  4
+{'q1': 0, 'q2': 1, 'q3': 1, 'q4': 0, 'q5': 0, 'q6': 0, 'q7': 0, 'q8': 1} Energy:  -2.0 Occurrences:  14
+{'q1': 1, 'q2': 1, 'q3': 0, 'q4': 0, 'q5': 0, 'q6': 0, 'q7': 0, 'q8': 1} Energy:  -2.0 Occurrences:  9
+{'q1': 0, 'q2': 0, 'q3': 0, 'q4': 1, 'q5': 1, 'q6': 0, 'q7': 0, 'q8': 1} Energy:  -2.0 Occurrences:  1
+{'q1': 1, 'q2': 1, 'q3': 0, 'q4': 0, 'q5': 1, 'q6': 0, 'q7': 0, 'q8': 1} Energy:  -1.0 Occurrences:  1
+{'q1': 0, 'q2': 1, 'q3': 1, 'q4': 0, 'q5': 0, 'q6': 1, 'q7': 1, 'q8': 0} Energy:  -1.0 Occurrences:  1
+{'q1': 0, 'q2': 1, 'q3': 0, 'q4': 0, 'q5': 1, 'q6': 0, 'q7': 0, 'q8': 1} Energy:  -1.0 Occurrences:  1
+{'q1': 1, 'q2': 0, 'q3': 1, 'q4': 0, 'q5': 0, 'q6': 1, 'q7': 1, 'q8': 0} Energy:  -1.0 Occurrences:  1
+{'q1': 0, 'q2': 1, 'q3': 0, 'q4': 0, 'q5': 0, 'q6': 1, 'q7': 1, 'q8': 0} Energy:  -1.0 Occurrences:  1
+{'q1': 1, 'q2': 0, 'q3': 0, 'q4': 1, 'q5': 0, 'q6': 0, 'q7': 0, 'q8': 1} Energy:  -1.0 Occurrences:  1
 '''
---
+++ hw3/fredkin.py
@@ -0,0 +1,83 @@
+from qiskit import QuantumRegister, ClassicalRegister, QuantumCircuit
+from qiskit import Aer, execute
+from qiskit.tools.visualization import plot_histogram
+from math import pi
+import matplotlib.pyplot as plt
+
+
+# 1/2 in exponent rotates pi/2, 1/4 in exponent rotates pi/4
+def fredkin(qc, control, targets):
+    qc.rz(pi/4, control[0])
+    
+    qc.cx(targets[1], targets[0])
+    qc.ry(-pi/2, targets[1])
+    
+    qc.rz(pi/4, targets[0])
+    qc.rz(pi/4, targets[1])
+
+    qc.cx(control[0], targets[0])
+    qc.cx(targets[0], targets[1])
+
+    qc.rz(-pi/4, targets[0])
+    qc.rz(pi/4, targets[1])
+
+    qc.cx(control[0], targets[0])
+    qc.cx(targets[0], targets[1])
+    qc.cx(control[0], targets[0])
+
+    qc.rz(-pi/4, targets[1])
+    qc.cx(targets[0], targets[1])
+
+    qc.rx(pi/2, targets[0])
+    qc.rz(-pi/4, targets[1])
+
+    qc.cx(control[0], targets[0])
+    qc.cx(targets[0], targets[1])
+
+    qc.rz(pi/2, targets[1])
+    qc.rx(pi/2, targets[0])
+    qc.rx(-pi/2, targets[1])
+
+    return qc
+    
+
+def create_circuit(control, targets, classical, vals):
+    qc = QuantumCircuit(control,targets,classical)
+    if vals[0]: # control val
+        qc.x(control[0])
+    if vals[1]: # target 0 val
+        qc.x(targets[0])
+    if vals[2]: # target 1 val
+        qc.x(targets[1])
+
+    qc = fredkin(qc, control, targets)
+    qc.measure(control[0],classical[0])
+    qc.measure(targets[0],classical[1])
+    qc.measure(targets[1],classical[2])
+    return qc
+
+
+def create_circuits():
+    control = QuantumRegister(1)  # input
+    targets = QuantumRegister(2) # output
+    c = ClassicalRegister(3)    
+    backend = Aer.get_backend('qasm_simulator')
+
+    first_qc = None
+    for i in range(8):
+        bin_string = bin(i)[2:].zfill(3)
+        vals = [int(b) for b in bin_string]
+        qc = create_circuit(control, targets, c, vals)
+        if i == 0:
+            first_qc = qc
+        job = execute(qc, backend, shots=100)
+        result = job.result()
+        print(result.get_counts())
+        #plot_histogram(result.get_counts())
+
+    first_qc.draw(output='mpl')
+    plt.show() # big number in qubit figure is "version"
+
+
+if __name__ == '__main__':
+    create_circuits()    
